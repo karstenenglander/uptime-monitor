deb688cf3496d4f5958867fdd746d7849e23ffc6
karstenenglander <karstenenglander@gmail.com>
2026-04-25 09:39:58 -0400

    Updated terraform to live in GCS

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 terraform {
+  backend "gcs" {
+    bucket = "englander-suite-tfstate"
+    prefix = "uptime-monitor"
+  }
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -15,5 +19,31 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_cloud_run_service" "default" {
+  name = "uptime-monitor-gcr"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/uptime-monitor"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloudbuild_trigger" "tf-trigger" {
+  github {
+    owner = "karstenenglander"
+    name  = "uptime-monitor"
+    push { branch = "^main$" }
+  }
+  filename = "cloudbuild.yaml"
 }
 
